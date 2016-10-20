0eb906625fc112dfdbb66709fe2a5ef42bd4d73c
Jeffreyumd <jalhassan@umassd.edu>
2016-10-20 17:10:32 -0400

    update on connect

--- connect.php
+++ connect.php
@@ -5,14 +5,10 @@ $mysql_user = "predqdtf";
 $mysql_password = "King71umassd";
 
 
-if(@mysql_connect($mysql_host,$mysql_user, $mysql_password) AND @mysql_select_db('predqdtf_portfolio'))
+if(!@mysql_connect($mysql_host,$mysql_user, $mysql_password)
+  AND @mysql_select_db('predqdtf_portfolio'))
 {
-
-}
-
-else
- {
   die("something went wrong");
- }
+}
 
  ?>
